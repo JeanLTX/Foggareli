305d237d26f6fdbebc2a92e317b87e74b0f55afb
JeanLTX <jeanluccaltx@gmail.com>
2026-03-02 11:36:00 -0300

    att endereços

--- script.js
+++ script.js
@@ -302,6 +302,8 @@ const DELIVERY_REGIONS = [
     { id: 'colinas', name: 'Colinas do Peró', fee: 4.00 },
     { id: 'jardim', name: 'Jardim Esperança, Tangará, Reserva, Nova Cabo Frio', fee: 6.00 },
     { id: 'porto', name: 'Porto do Carro, Vinhanteiro, Peró, Boca do Mato', fee: 8.00 },
+    { id: 'passaros', name: 'Condomínio dos pássaros', fee: 8.00 },
+    { id: 'bosque_pero', name: 'Condomínio Bosque do Peró', fee: 8.00 },
     { id: 'ogiva', name: 'Ogiva', fee: 10.00 },
     { id: 'cabo', name: 'Cabo Frio, Portinho, Novo Portinho', fee: 12.00 }
 ];
